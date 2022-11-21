ffb387fd92dfb1a55ffce2c728e513617192b146
colindickson <colin.dickson@gmail.com>
2022-11-21 16:20:16 -0500

    fix stats log message

--- stats.go
+++ stats.go
@@ -62,7 +62,7 @@ func (s *Stats) Start(each time.Duration) {
 					fields = append(fields, zap.Stringer("last_block", s.lastBlock))
 				}
 
-				s.logger.Info("substreams lidar stats", fields...)
+				s.logger.Info("substreams sink stats", fields...)
 			case <-s.Terminating():
 				break
 			}
